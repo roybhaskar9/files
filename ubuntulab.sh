5f8f1fa2770831db8e8e8962882f86da3d152d0a
roybhaskar9 <bhaskar@study9.com>
2017-10-18 20:04:21 +0530

    Update ubuntulab.sh

--- ubuntulab.sh
+++ ubuntulab.sh
@@ -3,6 +3,7 @@ cd ~
 sudo apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y virtualbox
+#wget http://download.virtualbox.org/virtualbox/5.1.30/virtualbox-5.1_5.1.30-118389~Ubuntu~xenial_amd64.deb
 sudo apt-get install -y vagrant
 wget https://releases.hashicorp.com/vagrant/2.0.0/vagrant_2.0.0_x86_64.deb
 sudo dpkg -i vagrant*.deb
