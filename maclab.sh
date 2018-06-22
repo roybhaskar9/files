4ba1abf4d1afd52b22cd058a299a4436915af346
roybhaskar9 <bhaskar@study9.com>
2018-06-22 20:29:08 +0530

    Update maclab.sh

--- maclab.sh
+++ maclab.sh
@@ -1,9 +1,3 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install wget tree
 # brew tap phinze/homebrew-cask && brew install brew-cask;
-brew cask install vagrant
-brew cask install virtualbox
-echo "#################################################################"
-echo "It will fail, so follow instructions at https://apple.co/2MO0abt"
-echo "Then run the virtosx.sh file"
-echo "#################################################################"
